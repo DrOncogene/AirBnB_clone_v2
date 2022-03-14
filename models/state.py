e68d2b89ebf24bbb3cec2741505c4b0e11f766b5
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-14 21:55:54 +0100

    added class attributes to represent columns in the db

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models import storage
+from models.base_model import BaseModel, Base
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column('name', String(128), nullable=False)
+
+    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+        cities = relationship('City', back_populates='state')
+    else:
+        @property
+        def cities(self):
+            all_cities = storage.all(City)
+            state_cities = [v for k, v in all_cities.items()
+                            if v.state_id == self.id]
+            return state_cities
