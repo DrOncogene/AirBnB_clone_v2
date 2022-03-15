1088299eef64a988ed04537c405ad9367c762f64
Rasheed Nasir <mypythtesting@gmail.com>
2022-03-15 09:58:39 +0100

    changed the place model to use db engine and make relations to city and user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import os
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -11,3 +13,7 @@ class User(BaseModel, Base):
     password = Column('password', String(128), nullable=False)
     first_name = Column('first_name', String(128))
     last_name = Column('last_name', String(128))
+
+    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+        places = relationship('Place', cascade='all, delete', 
+                              back_populates='user')
