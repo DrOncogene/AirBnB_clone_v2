1117ff0727253779e96d07b5b630fabe06d08e1f
Nasir Rasheed <mypythtesting@gmail.com>
2022-04-13 15:23:11 +0100

    refractored the function to handle already existing dir

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-from fabric.operations import local
+import os
 from datetime import datetime
 from fabric.decorators import task
+from fabric.api import local
 
 
 @task
 def do_pack():
     '''compresses web_static into an archive'''
-    local('mkdir -p versions/')
+    if not os.path.isdir('versions'):
+        os.mkdir("versions")
     now = datetime.now()
-    local('tar -czvf versions/web_static_{}{}{}{}{}{}.tgz web_static'.format(
-            now.year, now.month, now.day, now.hour, now.minute, now.second))
+    archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+        now.year, now.month, now.day, now.hour, now.minute, now.second)
+    archive_path = f"versions/{archive_name}"
+    local(f'tar -czvf {archive_path} web_static')
+    print("web_static packed: {} -> {}Bytes".format(
+          archive_path, os.stat(archive_path).st_size))
