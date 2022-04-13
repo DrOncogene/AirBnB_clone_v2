076a0b1ae287fd5bc6b6ddd4c4c5432aa74ca238
Nasir Rasheed <mypythtesting@gmail.com>
2022-04-13 22:43:16 +0100

    reverted test changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -34,7 +34,13 @@ def do_pack():
 
 @task
 def do_deploy(archive_path):
-    '''deploys web_static on to both web servers'''
+    """deploys web_static on to both web servers
+    Args:
+        archive_path(str): path to the static file archive
+
+    Return:
+        True if all ops succeeded or False
+    """
     if not ospath.exists(archive_path):
         return False
     archive_name = archive_path.split('/')[-1]
