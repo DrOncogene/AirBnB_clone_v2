7cb86b11b3beb607bdfdcc53d8dfe29dd0d8fb1c
Nasir Rasheed <mypythtesting@gmail.com>
2022-04-13 22:36:09 +0100

    updated script to reflect previous task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python3
 from os import path as ospath
 from datetime import datetime
-from fabric.decorators import task
-from fabric.network import connect
+from fabric.decorators import task, runs_once
 from fabric.api import local, run, put, env
 
 
 env.hosts, env.user = ['44.192.32.161', '34.139.191.175'], 'ubuntu'
+
+
+@runs_once
+def do_pack():
+    '''compresses web_static into an archive'''
+    if not os.path.isdir('versions'):
+        os.mkdir("versions")
+    now = datetime.now()
+    archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+        now.year,
+        now.month,
+        now.day,
+        now.hour,
+        now.minute,
+        now.second
+    )
+    archive_path = f"versions/{archive_name}"
+    try:
+        local(f'tar -czvf {archive_path} web_static')
+        print("web_static packed: {} -> {} Bytes".format(
+              archive_path, os.stat(archive_path).st_size))
+        return archive_path
+    except Exception:
+        return None
 
 
 @task
