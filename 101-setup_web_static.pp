7cf81ab38364d8506c9b5b2a898aba818948fed2
Nasir Rasheed <mypythtesting@gmail.com>
2022-04-14 10:00:33 +0100

    puppet manifest to provision a new server for file serving using /data

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,71 @@
+# configures a new web server to set up remote data directores
+# for test and releases
+
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+}
+
+exec { 'install nginx':
+  command => '/usr/bin/apt-get install -y --no-upgrade nginx',
+  require => Exec['apt-get update'],
+}
+
+exec { 'allow traffic':
+  command => "/usr/sbin/ufw allow 'Nginx HTTP'",
+  require => Exec['install nginx'],
+}
+
+exec { 'enable firewall':
+  command => '/usr/sbin/ufw enable',
+  require => Exec['allow traffic'],
+}
+
+exec { 'start nginx':
+  command => '/usr/sbin/service nginx start',
+  require => Exec['install nginx'],
+}
+
+file { 'releases directory':
+  path   => '/data/web_static/releases',
+  ensure => directory,
+}
+
+file { 'shared directory':
+  path   => '/data/web_static/shared/',
+  ensure => directory,
+}
+
+file { 'test directory':
+  path   => '/data/web_static/releases/test/',
+  ensure => directory,
+}
+
+file { 'create a test index.html':
+  path    => '/data/web_static/releases/test/index.html',
+  ensure  => 'file',
+  content => '<h1>testing nginx...</h1>',
+}
+
+file { 'current symlink':
+  path    => '/data/web_static/current',
+  ensure  => link,
+  target  => '/data/web_static/releases/test/',
+  require => File['test directory'],
+}
+
+file { 'data directory':
+  path    => '/data/',
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require => File['releases directory'],
+}
+
+exec { 'new endpoint':
+  command => '/usr/bin/sed -i -r "s|^(\s*)(location / \{)|\1location /hbnb_static {\n\1\1alias /data/web_static/current;\n\1\}\n\n\1\2|" /etc/nginx/sites-available/default',
+  require => File['current symlink'],
+}
+
+exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart',
+}
