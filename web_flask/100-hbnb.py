40de10378c5cfb388f756cfb2ef9031005b8c87f
Nasir Rasheed <mypythtesting@gmail.com>
2022-04-19 17:15:54 +0100

    pycodestyle compliant changes

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-display a list of all states and amenities grabbed from the db in the filters
+display a list of all states grabbed from the db
 """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
 
 
 app = Flask(__name__)
@@ -16,13 +17,15 @@ def teardown_session(exception):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
-    return render_template('10-hbnb_filters.html',
+    places = storage.all(Place).values()
+    return render_template('100-hbnb.html',
                            states=states,
-                           amenities=amenities)
+                           amenities=amenities,
+                           places=places)
 
 
 if __name__ == "__main__":
