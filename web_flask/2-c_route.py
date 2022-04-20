b71cbd91960ba885ea4a759ac4081709cde6e091
Nasir Rasheed <mypythtesting@gmail.com>
2022-04-20 08:23:48 +0100

    removed the f-string

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text: str):
     '''returns C + passed text'''
-    return f"C {text.replace('_', ' ')}"
+    return "C {}".format(text.replace('_', ' '))
 
 
 if __name__ == "__main__":
