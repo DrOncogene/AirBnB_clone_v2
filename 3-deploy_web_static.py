d797500f68b1454f0248c829eecd03be0361cfcb
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-06-14 04:25:30 +0100

    used sudo to execute remote tasks

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,7 +3,7 @@
 import os
 from datetime import datetime
 from fabric.decorators import task, runs_once
-from fabric.api import local, run, put, env
+from fabric.api import local, run, put, env, sudo
 
 
 env.hosts, env.user = ['3.236.69.47', '34.204.206.251'], 'ubuntu'
@@ -29,7 +29,8 @@ def do_pack():
         print("web_static packed: {} -> {} Bytes".format(
               archive_path, os.stat(archive_path).st_size))
         return archive_path
-    except Exception:
+    except Exception as err:
+        print(err)
         return None
 
 
@@ -48,21 +49,22 @@ def do_deploy(archive_path):
     release_dir = archive_name.split('.')[0]
     try:
         put(archive_path, '/tmp/')
-        run("mkdir -p /data/web_static/releases/{}".format(
+        sudo("mkdir -p /data/web_static/releases/{}".format(
              archive_name.split('.')[0]))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
+        sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
             archive_name, release_dir))
-        run('rm /tmp/{}'.format(archive_name))
-        run('mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}/'.format(release_dir))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+        sudo('rm /tmp/{}'.format(archive_name))
+        sudo('mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/'.format(release_dir, release_dir))
+        sudo('rm -rf /data/web_static/releases/{}/web_static'.format(
              release_dir))
-        run('rm -rf /data/web_static/current')
-        run('ln -s -f /data/web_static/releases/{}/\
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s -f /data/web_static/releases/{}/\
             /data/web_static/current'.format(release_dir))
         print("New version deployed!")
         return True
-    except Exception:
+    except Exception as err:
+        print(err)
         return False
 
 
